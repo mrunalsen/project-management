af1c1356dfd4d12e04a00d91f5a284192fa51a95
mrunalsen <mrunalpatel2500@gmail.com>
2022-08-30 19:01:40 +0530

    UI changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MasterComponent } from './master/master.component';
+import { SidebarComponent } from './master/sidebar/sidebar.component';
+import { HeaderComponent } from './master/header/header.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MasterComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+  ],
+  exports: [
+    SidebarComponent
   ]
 })
 export class CoreModule { }
