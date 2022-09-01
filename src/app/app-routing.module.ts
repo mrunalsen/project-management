fe5510bbb9efede3bf8340144ecf5c2f9be8a7c0
mrunalsen <mrunalpatel2500@gmail.com>
2022-09-01 19:31:06 +0530

    Sidebar Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './core/master/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {path:'header', component: HeaderComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
